d2bbf89a6094d145b69df9ddf14c6a9b9bd6f548
yulen59 <j.delemarle@gmail.com>
2021-03-25 15:48:34 +0100

    modify style.css and javaS.js

--- javaS.js
+++ javaS.js
@@ -48,11 +48,6 @@ const contents = [
     }
 ];
 
-<<<<<<< HEAD
-=======
-
-const contents = ["backEnd","cars","images","failCss"];
->>>>>>> cf304d59eecdcdb81ea250cb01ac706faba8d2b7
 for(let i = 0; i < contents.length;i++) {
    appendContent(contents[i]);
   
